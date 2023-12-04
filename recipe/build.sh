be9818179eef7d513a3e8267491f606763e610e8
juacrumar <juacrumar@lairen.eu>
2023-12-04 19:16:41 +0100

    recover mistakenly removed line

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./config/
 ./configure --prefix=$PREFIX
 make -j
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
