bbd35a3dbb9d8a30631d891eb93d9070e94518ca
juacrumar <juacrumar@lairen.eu>
2023-12-04 23:13:41 +0100

    swap arm64 and ox

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-./configure --prefix=$PREFIX
-make -j
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-make check
-fi
-make install
 
-cd wrappers
-make
+if [[ $HOST == *arm64* ]]; then
+    # Get an updated config.sub and config.guess
+    cp $BUILD_PREFIX/share/gnuconfig/config.* ./config/
+fi
+ 
+./configure --prefix=$PREFIX
+
+make -j
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    make check
+fi
+
 make install
