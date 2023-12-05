8b2066f5eb25cc73b1720cf7094285ab330e53ec
juacrumar <juacrumar@lairen.eu>
2023-12-05 08:17:01 +0100

    leva only python 3.11 for testing ; try to get more info on the failure

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,6 +13,8 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}
 fi
 make install
 
-cd wrappers
-make
-make install
+# cd wrappers
+# make
+# make install
+
+python -c "import lhapdf"
