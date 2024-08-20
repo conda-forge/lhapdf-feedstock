f62e90044b613771fadd1c13a32f5500763ab05d
Matthew Feickert <matthew.feickert@cern.ch>
2024-08-20 01:59:20 -0500

    MNT: Remove cython upper bound and use lower bound of v3.0.0
    * Use GitLab as the source for a consistency between development and
      build source as hepforge releases have configure scripts pre-buit which
      need to be rebuilt in certain cases, so it is preferable to build everything
      consistently.
       - c.f. https://gitlab.com/hepcedar/lhapdf
    * Add automake, autoconf, and libtool as 'build' requirements. These are all
      necessary for working with autotools (e.g. autoconf and autoreconf).
    * Use 'autoreconf --install --force' for all builds to generate the
      configure script.
    * Apply the osx_patch.diff patch for macOS selectively by defining it
      as a patch in recipe/meta.yaml.
    * Add cython >=0.24 as both a 'host' requirement and as a 'build' requirement
      for cross compiles. This removes the upper bound allowing for Cython v3.x
      to be used, but only enforces the lower bound of Cython v0.24 that configure
      checks for.
    * Use cython to build the sources for the Python bindings for all builds as
      this can be useful in cases like new Python versions
    * Use cython's '--include-dir' option to properly pick up the include files
      for the C++ file generation for the Python bindings.
    
      ```
      -I INCLUDE_PATH, --include-dir INCLUDE_PATH
                            Search for include files in named directory (multiple
                            include directories are allowed).
      ```
    
    * Add CXXFLAGS to configure which affects 'lhapdf-config --cxxflags'.
    * Add reference to install docs on why '-Qunused-arguments' are used
      for macOS builds.
    * Bump build number.
    
    Co-authored-by: Chris Burr <christopher.burr@cern.ch>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 if [[ $HOST == *apple* ]]; then
-    # Apply patch to build.py.in (autoreconf might not be needed?)
-    patch -p1 < ${RECIPE_DIR}/osx_patch.diff
-    # These two flags are mentioned in the LHAPDF docs
+    # Bug for macOS as of lhapdf v6.1.1+
+    # https://gitlab.com/hepcedar/lhapdf/-/blob/c5b048f5794e483d85801d1ae39f42aaa46999dd/INSTALL#L162-164
     export CFLAGS=-Qunused-arguments
     export CPPFLAGS=-Qunused-arguments
+
+    # As using autotools
+    # https://conda-forge.org/blog/2020/10/29/macos-arm64/#how-to-add-a-osx-arm64-build-to-a-feedstock
+    if [[ $HOST == *arm64-apple* ]]; then
+        # Get an updated config.sub and config.guess
+        cp $BUILD_PREFIX/share/gnuconfig/config.* .
+    fi
 fi
 
-if [[ $HOST == *arm64* ]]; then
-    # Get an updated config.sub and config.guess
-    cp $BUILD_PREFIX/share/gnuconfig/config.* ./config/
-    # And autoreconf!
-    autoreconf -i -f
-    (cd wrappers/python && cython --cplus lhapdf.pyx)
-fi
+autoreconf --install --force
+(cd wrappers/python && cython --include-dir . --cplus lhapdf.pyx)
 
-./configure --prefix=$PREFIX
+./configure --help
 
-make -j
+./configure --prefix=$PREFIX CXXFLAGS="${CXXFLAGS}"
+
+make --jobs="${CPU_COUNT}"
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
     make check
 fi
